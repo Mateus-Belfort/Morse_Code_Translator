ed97b1d97866aab749bd523b81e1a820d7edc1de
Mateus-Belfort <mateusaguia96@gmail.com>
2022-10-31 20:17:44 -0300

    Atualização de Codigo Fonte
    alteração do codigo fonte JS, troca de icones e melhoria na estilização

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,61 @@
-//Tradutor de código Morse- By Mateus Belfort (https://github.com/Mateus-Belfort)
-//Inspirado no projeto de Luke Watson (luke@lukewatson.f2s.com)
+//Morse code converter- By Luke Watson (luke@lukewatson.f2s.com)
 //Script featured on JK (http://javascriptkit.com)
 //Visit http://javascriptkit.com for this script and more
 
+var charCodes=new Array(36); charCodes["a"]=". _";
+charCodes["b"]="_ . . .";
+charCodes["c"]="_ . _ .";
+charCodes["d"]="_ . .";
+charCodes["e"]=".";
+charCodes["f"]=". . _ .";
+charCodes["g"]="_ _ .";
+charCodes["h"]=". . . .";
+charCodes["i"]=". .";
+charCodes["j"]=". _ _ _";
+charCodes["k"]="_ . _";
+charCodes["l"]=". _ . .";
+charCodes["m"]="_ _";
+charCodes["n"]="_ .";
+charCodes["o"]="_ _ _";
+charCodes["p"]=". _ _ .";
+charCodes["q"]="_ _ . _";
+charCodes["r"]=". _ .";
+charCodes["s"]=". . .";
+charCodes["t"]="_";
+charCodes["u"]=". . _";
+charCodes["v"]=". . . _";
+charCodes["w"]=". _ _";
+charCodes["x"]="_ . . _";
+charCodes["y"]="_ . _ _";
+charCodes["z"]="_ _ . .";
+charCodes["1"]=". _ _ _ _";
+charCodes["2"]=". . _ _ _";
+charCodes["3"]=". . . _ _";
+charCodes["4"]=". . . . _";
+charCodes["5"]=". . . . .";
+charCodes["6"]="_ . . . .";
+charCodes["7"]="_ _ . . .";
+charCodes["8"]="_ _ _ . .";
+charCodes["9"]="_ _ _ _ .";
+charCodes["0"]="_ _ _ _ _";
+var temp=''
 
-var charCodes=new Array(36); 
-    charCodes["a"]=". _ ";
-    charCodes["b"]="_ . . .";
-    charCodes["c"]="_ . _ .";
-    charCodes["d"]="_ . .";
-    charCodes["e"]=".";
-    charCodes["f"]=". . _ .";
-    charCodes["g"]="_ _ .";
-    charCodes["h"]=". . . .";
-    charCodes["i"]=". .";
-    charCodes["j"]=". _ _ _";
-    charCodes["k"]="_ . _";
-    charCodes["l"]=". _ . .";
-    charCodes["m"]="_ _";
-    charCodes["n"]="_ .";
-    charCodes["o"]="_ _ _";
-    charCodes["p"]=". _ _ .";
-    charCodes["q"]="_ _ . _";
-    charCodes["r"]=". _ .";
-    charCodes["s"]=". . .";
-    charCodes["t"]="_";
-    charCodes["u"]=". . _";
-    charCodes["v"]=". . . _";
-    charCodes["w"]=". _ _";
-    charCodes["x"]="_ . . _";
-    charCodes["y"]="_ . _ _";
-    charCodes["z"]="_ _ . .";
-    charCodes["1"]=". _ _ _ _";
-    charCodes["2"]=". . _ _ _";
-    charCodes["3"]=". . . _ _";
-    charCodes["4"]=". . . . _";
-    charCodes["5"]=". . . . .";
-    charCodes["6"]="_ . . . .";
-    charCodes["7"]="_ _ . . .";
-    charCodes["8"]="_ _ _ . .";
-    charCodes["9"]="_ _ _ _ .";
-    charCodes["0"]="_ _ _ _ _";
-    var temp=''
-    
-    function codificar() {
-        if(document.getElementById("normal").value.length < 1){
-            alert('Por favor, Digite algo para traduzir!');
-            document.getElementById("normal").focus();
-            return false
-        }
+function encode() {
+document.morsecode.chars.value=document.morsecode.chars.value.toLowerCase();
+document.morsecode.codebox.value="";
+temp=''
+var chars=document.morsecode.chars.value.split("");
 
-    document.elementos.letras.value=document.elementos.letras.value.toLowerCase();
-    document.elementos.result.value="";
-    temp=''
-    var letras=document.elementos.letras.value.split("");
-    
-    for (a=0; a<letras.length; a++) {
-    if (letras[a]!=" ") {
-    if (window.charCodes[letras[a]]) {
-    document.elementos.result.value+=charCodes[letras[a]]+"    ";
-    temp+=letras[a]+" = "+charCodes[letras[a]]+" \n";
-    }
-    else
-    temp+=letras[a]+"=( Caractere não reconhecido )\n";
-    }
-    else temp+="\n";
-    }
-    
-    document.elementos.result.value+="\n\n\n Explicão: \n\n"+temp
-    }
-
-
+for (a=0; a<chars.length; a++) {
+if (chars[a]!=" ") {
+if (window.charCodes[chars[a]]) {
+document.morsecode.codebox.value+=charCodes[chars[a]]+"    ";
+temp+=chars[a]+"="+charCodes[chars[a]]+"\n";
+}
+else
+temp+=chars[a]+"=(None)\n";
+}
+else temp+="\n";
+}
+document.morsecode.codebox.value+="\n\n\nEXPLANATION:\n\n"+temp
+}
